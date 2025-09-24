5f21f51c3b9b3f77dee400a64d8c4945ba7a9b09
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2025-09-24 18:23:36 +0300

    feat: add HomeSwiper component for enhanced carousel functionality

--- src/app/[locale]/(main-app)/page.tsx
+++ src/app/[locale]/(main-app)/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import BannerSection from "./components/BannerSection";
 import AboutSection from "./components/AboutSection";
 import MediaSection from "./components/MediaSection";
+import HomeSwiper from "./components/HomeSwiper";
 const slides = [
   {
     image: "/assets/carousel.webp",
@@ -35,7 +36,7 @@ const Home = () => {
       {/* Hero Section */}
       <BannerSection />
       <AboutSection />
-
+      <HomeSwiper slides={slides} autoplayMs={5500} />
       <MediaSection />
     </>
   );
